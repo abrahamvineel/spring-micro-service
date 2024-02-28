6ecf1356e68ef80ce02149515b5301489a6afea0
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-02-28 17:53:18 -0500

    eureka clients

--- spring-micro-services-new/order-service/src/main/java/com/pro/orderservice/service/OrderService.java
+++ spring-micro-services-new/order-service/src/main/java/com/pro/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -36,8 +36,8 @@ public class OrderService {
 
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode)
                 .toList();
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:9192/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://invenory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes)
                                 .build())
                 .retrieve()
