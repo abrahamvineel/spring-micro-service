abb81f08a79a17d4efa3b0dd87580b6209258ebb
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-03-07 20:44:40 -0500

    format file

--- spring-micro-services-new/order-service/src/main/java/com/pro/orderservice/service/OrderService.java
+++ spring-micro-services-new/order-service/src/main/java/com/pro/orderservice/service/OrderService.java
@@ -23,6 +23,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
+
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
