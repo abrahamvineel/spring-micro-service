5bf12e3a9ebcf374bfea04801e491eb9fcefbfe6
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-03-14 19:56:37 -0400

    discovery server security wip

---
+++ spring-micro-services-new/discovery-server/src/main/java/org/example/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package org.example.config;
+
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("eureka").password("password")
+                .authorities("USER");
+    }
+}
