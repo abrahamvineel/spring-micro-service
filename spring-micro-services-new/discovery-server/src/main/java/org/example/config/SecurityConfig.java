2faa3c0f53053451e1505c053d53d92b5e465427
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-03-14 20:03:50 -0400

    removed unused imports

--- spring-micro-services-new/discovery-server/src/main/java/org/example/config/SecurityConfig.java
+++ spring-micro-services-new/discovery-server/src/main/java/org/example/config/SecurityConfig.java
@@ -1,14 +1,9 @@
 package org.example.config;
 
-import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
